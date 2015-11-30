5b3b63abba0dab20498571e5498ed7fd214f6571
Chris Liang <chrisliang98@gmail.com>
2015-11-30 01:18:00 -0500

    removed merge line

--- utils.py
+++ utils.py
@@ -9,12 +9,7 @@ def findMostCommonElement(l):
         else:
             wordCounter[word]=1
     sortedDict =  sorted(wordCounter.items(), key=operator.itemgetter(1))
-<<<<<<< HEAD
     return sortedDict[-1][0]
-=======
-    print sortedDict
-    return sortedDict[-1]
->>>>>>> 65d66b6ed1bb8425d8c63f32728f3b474b7154b9
 
 #takes data from a website (done in names.py)
 #returns the list of most common first names
