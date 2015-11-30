769bc2eb170ab7d988719e7607a013deb4d17192
lihelennn <helen82198@gmail.com>
2015-11-30 00:21:08 -0500

    added comments and cleaned up code

--- utils.py
+++ utils.py
@@ -9,12 +9,18 @@ def findMostCommonElement(l):
         else:
             wordCounter[word]=1
     sortedDict =  sorted(wordCounter.items(), key=operator.itemgetter(1))
+    print sortedDict
     return sortedDict[-1]
 
+#takes data from a website (done in names.py)
+#returns the list of most common first names
+#not used
 def compareNames(l):
     namesList = []
     for word in l:
         if word in names.getNames():
             namesList.append(word)
     return namesList
+
+
     
